9f01b61136800a531b3093ee3cfc2797efb1dee5
abdelllatif <haissouneabdellatif749@gmail.com>
2025-12-14 00:33:16 +0100

    I add the dependecy of jsonwebtoken

---
+++ Backend-JobStream/src/main/java/com/job/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.job.security;
+
+import com.job.entity.User;
+import com.job.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
