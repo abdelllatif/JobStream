109bb55884aef03755e1df0a5b33c60952d90849
abdelllatif <haissouneabdellatif749@gmail.com>
2025-12-14 00:17:05 +0100

    I add the user mapper methods

--- Backend-JobStream/src/main/java/com/job/mapper/UserMapper.java
+++ Backend-JobStream/src/main/java/com/job/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package com.job.mapper;
 
-public class UserMapper {
+import com.job.dto.request.UserRequestDTO;
+import com.job.dto.response.UserResponseDTO;
+import com.job.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    User toEntity(UserRequestDTO dto);
+
+    UserResponseDTO toResponse(User user);
 }
