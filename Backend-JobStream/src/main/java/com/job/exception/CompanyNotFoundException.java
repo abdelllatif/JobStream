b43382bfd264134665b65e117067a4dbeb8e7bf6
abdelllatif <haissouneabdellatif749@gmail.com>
2025-12-22 11:30:53 +0100

    here I add the others classes of exeptions

---
+++ Backend-JobStream/src/main/java/com/job/exception/CompanyNotFoundException.java
@@ -0,0 +1,12 @@
+package com.job.exception;
+
+public class CompanyNotFoundException extends RuntimeException {
+    public CompanyNotFoundException(String message) {
+        super(message);
+    }
+
+    public CompanyNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
